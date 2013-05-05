702e2a6c61a54dc72f68738e1e1a9af1dbac96fe
Colin Gemmell <pythonandchips@gmail.com>
2013-05-05 23:30:31 +0100

    combined tasks

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,20 +1,24 @@
 /*global module:false*/
 module.exports = function(grunt) {
 
+  var bowerPath = 'bower_components';
+  var sourcePath = 'js/**/*.js';
+
   // Project configuration.
   grunt.initConfig({
     pkg: 'angular-todomvc',
     jshint: {
-      files: ['Gruntfile.js', 'js/**/*.js', 'test/**/*.js']
+      files: ['Gruntfile.js', sourcePath, 'test/**/*.js']
     },
     jasmine : {
-      src : ['bower_components/angular/*.js', 'bower_components/angular-mocks/*.js', 'bower_components/todomvc-common/*.js', 'js/**/*.js'],
+      src : [bowerPath + '/angular/*.js', bowerPath + '/angular-mocks/*.js', bowerPath + '/todomvc-common/*.js', sourcePath],
       options: {
         specs : 'test/unit/**/*.js'
       }
     }
   });
 
+  grunt.registerTask('default', ['jshint', 'jasmine']);
   // These plugins provide necessary tasks.
   // Default task.
   grunt.loadNpmTasks('grunt-contrib-jshint');
