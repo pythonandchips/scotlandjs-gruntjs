ec241c80fb75784ddb48d72fa3dabe5410ad8b5d
Colin Gemmell <pythonandchips@gmail.com>
2013-05-05 23:06:50 +0100

    blank grunt file

---
+++ Gruntfile.js
@@ -0,0 +1,12 @@
+/*global module:false*/
+module.exports = function(grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+  });
+
+  // These plugins provide necessary tasks.
+  // Default task.
+  grunt.registerTask('default', ['jshint', 'jasmine']);
+
+};
