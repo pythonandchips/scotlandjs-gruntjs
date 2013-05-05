ded1745aada284f992bc0c6f1899270bbe01f55d
Colin Gemmell <pythonandchips@gmail.com>
2013-05-06 00:30:01 +0100

    added staging deploy

--- Gruntfile.js
+++ Gruntfile.js
@@ -40,7 +40,48 @@ module.exports = function(grunt) {
       },
       livereload: {
         files: ['index.html', bowerPath + '/todomvc-common/base.css', bowerPath + '/angular/*.js', bowerPath + '/angular-mocks/*.js', bowerPath + '/todomvc-common/*.js', sourcePath, 'test/**/*.js'],
-        tasks: ['livereload'],
+        tasks: ['livereload']
+      }
+    },
+    s3: {
+      options: {
+        key: process.env.AWSKEY,
+        secret: process.env.AWSSECRET,
+        bucket: 'scotlandjs',
+        access: 'public-read'
+      },
+      staging: {
+        // These options override the defaults
+        upload: [
+          {
+            src: bowerPath + '/angular/*',
+            dest: '/bower_components/angular',
+          },
+          {
+            src: bowerPath + '/todomvc-common/*',
+            dest: '/bower_components/todomvc-common',
+          },
+          {
+            src: 'js/controllers/*.js',
+            dest: '/js/controllers',
+          },
+          {
+            src: 'js/directives/*.js',
+            dest: '/js/directives',
+          },
+          {
+            src: 'js/services/*.js',
+            dest: '/js/services',
+          },
+          {
+            src: 'js/app.js',
+            dest: '/js/app.js',
+          },
+          {
+            src: 'index.html',
+            dest: '/index.html',
+          }
+        ]
       }
     }
   });
@@ -48,11 +89,13 @@ module.exports = function(grunt) {
   grunt.registerTask('watch', ['regarde:dev']);
   grunt.registerTask('default', ['jshint', 'jasmine']);
   grunt.registerTask('reload', ['livereload-start', 'connect', 'regarde:livereload']);
+  grunt.registerTask('staging', ['jshint', 'jasmine', 's3:staging']);
   // These plugins provide necessary tasks.
   // Default task.
   grunt.loadNpmTasks('grunt-contrib-jshint');
   grunt.loadNpmTasks('grunt-contrib-jasmine');
   grunt.loadNpmTasks('grunt-contrib-connect');
   grunt.loadNpmTasks('grunt-contrib-livereload');
+  grunt.loadNpmTasks('grunt-s3');
   grunt.loadNpmTasks('grunt-regarde');
 };
