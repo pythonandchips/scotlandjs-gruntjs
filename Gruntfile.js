4e0a7920f461169319bea8927f89e071e794a408
Colin Gemmell <pythonandchips@gmail.com>
2013-05-05 23:12:30 +0100

    add jshint

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,9 +3,13 @@ module.exports = function(grunt) {
 
   // Project configuration.
   grunt.initConfig({
+    pkg: 'angular-todomvc',
+    jshint: {
+      files: ['Gruntfile.js', 'js/**/*.js', 'test/**/*.js']
+    }
   });
 
   // These plugins provide necessary tasks.
   // Default task.
-
+  grunt.loadNpmTasks('grunt-contrib-jshint');
 };
